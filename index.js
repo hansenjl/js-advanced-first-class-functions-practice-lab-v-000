255035a09683e9711ec483f2f2e831b071f30e62
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-04-13 18:31:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ function driversByRevenue(drivers){
     return a.revenue - b.revenue;
   })
 }
+
+
+function driversByName(drivers){
+  return drivers.slice().sort(function(a,b){
+    return a.name - b.name;
+  })
+}
